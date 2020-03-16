2c260c89b3297819a48eb5f29ef8fd1e2d604f07
robjlucas <rob@newleftreview.org>
2020-03-16 09:39:29 +0000

    Install Annotate gem for annotated routes and models

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,3 +1,20 @@
+# == Schema Information
+#
+# Table name: users
+#
+#  id                 :bigint           not null, primary key
+#  created_at         :datetime         not null
+#  updated_at         :datetime         not null
+#  email              :string           not null
+#  encrypted_password :string(128)      not null
+#  confirmation_token :string(128)
+#  remember_token     :string(128)      not null
+#
+# Indexes
+#
+#  index_users_on_email           (email)
+#  index_users_on_remember_token  (remember_token)
+#
 class User < ApplicationRecord
   include Clearance::User
 
